a484dc66a4a4c063722f8b695068dfa13a23b8c1
Cornel-Cristian Filip <filipcornelcristian@gmail.com>
2022-12-12 15:03:07 +0200

    added Footer component and styles

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import styles from './Footer.module.css';
+
+export const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <footer className={styles.footer}>
+      <a href='https://github.com/psyhedeliq'>Cornel-Cristian Filip - Github</a>
+      <br />
+      {currentYear}
+    </footer>
+  );
+};
