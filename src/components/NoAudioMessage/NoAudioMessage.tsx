5e15f5771d0780eab49c805172ce165ca3e31794
Cornel-Cristian Filip <filipcornelcristian@gmail.com>
2022-12-20 13:52:23 +0200

    added NoAudioMessage component

---
+++ src/components/NoAudioMessage/NoAudioMessage.tsx
@@ -0,0 +1,10 @@
+export const NoAudioMessage = () => {
+  return (
+    <div>
+      <p>Sorry, it's not gonna work :-(</p>
+      <p>
+        Seems like your Browser doesn't support <code>Audio API</code>
+      </p>
+    </div>
+  );
+};
